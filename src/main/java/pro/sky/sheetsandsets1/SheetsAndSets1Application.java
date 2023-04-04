eed2053199d54d453a69cd0b4384289c9639fce9
mycomputer <Danil12.07.2012@gail.com>
2023-04-04 18:39:50 +0300

    Sheets and sets

---
+++ src/main/java/pro/sky/sheetsandsets1/SheetsAndSets1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.sheetsandsets1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetsAndSets1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheetsAndSets1Application.class, args);
+    }
+
+}
